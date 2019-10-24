00ca46586fdb4bf7c529cfb80acecfda9e5b25c9
GuilhermeKobori <koboriguilherme@gmail.com>
2019-10-23 21:32:49 -0300

    Added green slime animation and dragable functionalities

---
+++ Monsters/GreenSlime/GreenSlime.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+var can_drag = false
+
+func _ready():
+	$AnimationPlayer.play("Groovin'")
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		can_drag = event.pressed
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_drag:
+		position = get_global_mouse_position()
