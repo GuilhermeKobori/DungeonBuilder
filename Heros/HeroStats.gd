2f79539088d2415d4aa770d365220a3f7979bc4e
pedrobairao <pedro.bairao@gmail.com>
2019-10-30 20:23:55 -0300

    Monsters and Heros Stats

---
+++ Heros/HeroStats.gd
@@ -0,0 +1,16 @@
+extends Resource
+
+class_name HeroStats
+
+export var job_name : String = "job"
+#export var name : String
+
+export var description : String
+export var type : String
+export var max_health : int
+export var attack : int
+export var atk_speed : float
+export var speed : float
+export var reach : int
+
+export var drop : int
---
+++ Monsters/MonsterStats.gd
@@ -0,0 +1,14 @@
+extends Resource
+
+class_name MonsterStats
+
+export var job_name : String = "job"
+
+export var description : String
+export var type : String
+export var max_health : int
+export var cost : int
+export var attack : int
+export var atk_speed : float
+export var speed : float
+export var reach : int
