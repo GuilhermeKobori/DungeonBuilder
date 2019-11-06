2f85a3ad61c2a5372556ee100ca2b2d61bb71b4b
Michel Chieregato <michel.chieregato@gmail.com>
2019-11-06 20:42:20 -0200

    Start doing things

--- Monsters/Lesma/Lesma.gd
+++ Monsters/Lesma/Lesma.gd
@@ -1,25 +1,29 @@
 extends KinematicBody2D
 
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var monster_name = 'Lesma'
+var life = 10
+var attack = 2
+var speed = 0.3
 var sent = 1
-var attack = 3;
-var life = 10;
-var defense = 2;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(name)
 	set_physics_process(true)
-	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _physics_process(delta):
-	var new_offset = get_parent().get_unit_offset() + delta * sent * 0.3
+	var new_offset = get_parent().get_unit_offset() + delta * sent * speed
 	if sent == 1 and new_offset >= 0.99:
 		sent = -1
 		get_parent().set_unit_offset(0.99)
