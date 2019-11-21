d0e0a5bcbbdca38349a77209549bcfdf2ae1a26a
akaFTS <gustavo84171@hotmail.com>
2019-11-21 15:57:59 -0300

    Fix robson spawn

--- Screens/Level2.gd
+++ Screens/Level2.gd
@@ -6,18 +6,13 @@ var time_elapsed = 0.0
 var greenSlimeScn = load("res://Monsters/GreenSlime/GreenSlime.tscn")
 var monsterScn = load("res://Monsters/Monster.tscn")
 
-var robson_spawn_times = [0.5, 3.0, 5.0, 7.0, 10.0]
+var robson_spawn_times = [600.0]
 onready var path : Path2D = $Path2D
 
 
 func _ready() -> void:
-	set_physics_process(false)
-	$Interface/Play.connect("pressed", self, "start_level")
-	$Interface.connect("create_monster", self, "bought_monster")
-	
-func start_level():
-	$Interface/Play.hide()
 	set_physics_process(true)
+	$Interface.connect("create_monster", self, "bought_monster")
 
 func bought_monster(name, cost):
 	print("Bought Monster: " + name + " - Cost $" + str(cost))
